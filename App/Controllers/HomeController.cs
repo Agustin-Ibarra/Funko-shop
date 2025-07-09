8aad9c062bce9e127202b961d24960eede7ed5e6
Agustin-Ibarra <agustingsoc@gmail.com>
2025-07-08 23:31:02 -0300

    arreglar errores en HomeController

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -1,19 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using Funko_shop.Repository;
 
 namespace Funko_shop.Controllers;
 
 public class HomeController : Controller
 {
-  private readonly ItemRepository _itemRepository;
-  private readonly ILogger<HomeController> _logger;
-
-  public HomeController(ILogger<HomeController> logger, ItemRepository repository)
-  {
-    _logger = logger;
-    _itemRepository = repository;
-
-  }
 
   [HttpGet]
   public IActionResult Index()
