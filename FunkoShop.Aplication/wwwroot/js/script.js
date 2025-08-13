cefb880fb2adbc559c59f2bbaed5c6f7dda6b4ec
Agustin-Ibarra <agustingsoc@gmail.com>
2025-08-13 16:53:22 -0300

    desarrollar diseño responsive para la seccion shop

--- FunkoShop.Aplication/wwwroot/js/script.js
+++ FunkoShop.Aplication/wwwroot/js/script.js
@@ -1,8 +1,24 @@
 const body = document.querySelector("body");
+const menu = document.querySelector("nav");
+const options = document.querySelector(".options");
+const optionsIcon = document.querySelector(".options-icon");
+
 body.addEventListener("click",(e)=>{
-  if(e.target.matches(".menu-btn") || e.target.matches(".burger") || e.target.matches(".close-menu") || e.target.matches(".close")){
-    const menu = document.querySelector("nav");
-    menu.classList.toggle("show");
-    console.log("menu");
+  if(e.target.matches(".menu-btn") || e.target.matches(".burger")){
+    menu.classList.add("show");
+    optionsIcon.classList.remove("show");
+    optionsIcon.classList.add("invisible");
+  }
+  else if(e.target.matches(".close")){
+    menu.classList.remove("show");
+    optionsIcon.classList.remove("invisible");
+  }
+  else if(e.target.matches(".options-icon")){
+    console.log("options");
+    options.classList.add("show");
+    menu.classList.remove("show");
+  }
+  else if(e.target.matches(".icon-back") || e.target.matches(".search")){
+    options.classList.remove("show");
   }
 });
